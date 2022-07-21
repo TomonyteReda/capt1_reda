80e4c02b496ad0c1d5f66a79f6dea82a1b0c50f9
TomonyteReda <reda.tomonyte@gmail.com>
2022-07-21 20:23:42 +0300

    updated translations and admin site

--- mysite/myreport/admin.py
+++ mysite/myreport/admin.py
@@ -1,13 +1,29 @@
 from django.contrib import admin
 from .models import Activity, DataFile
+from django.utils.translation import gettext_lazy as _
 
 
 class ActivityAdmin(admin.ModelAdmin):
-    list_display = ('log_date', 'quantity_impressions', 'quantity_clicks', 'user')
+    list_display = ('log_date', 'quantity_impressions', 'quantity_clicks', 'user', 'get_upload_date')
+    list_filter = ('log_date', 'user', 'data_file__upload_date')
+    fieldsets = (
+        ('General', {'fields': ('user', 'log_date')}),
+        ('Activities', {'fields': ('quantity_impressions', 'quantity_clicks')}),
+    )
+
+    search_fields = ('log_date', 'user__username')
+
+    def get_upload_date(self, obj):
+        return obj.data_file.upload_date
+
+    get_upload_date.short_description = _('Upload Date')
 
 
 class DataFileAdmin(admin.ModelAdmin):
     list_display = ('hash_checksum', 'file_contents', 'user', 'upload_date')
+    list_filter = ('user', 'upload_date')
+
+    search_fields = ('upload_date', 'user__username')
 
 
 admin.site.register(DataFile, DataFileAdmin)
--- mysite/myreport/models.py
+++ mysite/myreport/models.py
@@ -4,25 +4,31 @@ from django.utils.translation import gettext_lazy as _
 
 
 class DataFile(models.Model):
-    hash_checksum = models.CharField('hash checksum', max_length=100, null=True, blank=True)
-    file_contents = models.FileField('file contents', null=True, blank=True, upload_to="user_files")
-    user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, blank=True)
-    upload_date = models.DateField('upload date', auto_now_add=True)
+    hash_checksum = models.CharField(_('hash checksum'), max_length=100, null=True, blank=True)
+    file_contents = models.FileField(_('file contents'), null=True, blank=True, upload_to="user_files")
+    user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, blank=True, verbose_name=_('user'))
+    upload_date = models.DateField(_('upload date'), auto_now_add=True)
 
     def __str__(self):
-        return f'{self.hash_checksum} {self.user} {self.upload_date}'
+        return f'{self.user} {self.upload_date}'
+
+    class Meta:
+        verbose_name = _('Data File')
+        verbose_name_plural = _('Data Files')
 
 
 class Activity(models.Model):
-    log_date = models.DateTimeField('date', null=True, blank=True)
-    quantity_impressions = models.IntegerField('impressions', default=0)
-    quantity_clicks = models.IntegerField('clicks', default=0)
+    log_date = models.DateTimeField(_('date'), null=True, blank=True)
+    quantity_impressions = models.IntegerField(_('impressions'), default=0)
+    quantity_clicks = models.IntegerField(_('clicks'), default=0)
     data_file = models.ForeignKey('DataFile', on_delete=models.CASCADE, null=True, related_name='activity')
-    user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, blank=True, verbose_name=_('user'))
 
     def __str__(self):
-        return f'uploaded on {self.upload_date}: ' \
-               f'{self.log_date} {self.quantity_impressions} {self.quantity_clicks} by user {self.user}'
+        return f'Log Date: {self.log_date} ' \
+               f'Impressions:{self.quantity_impressions} ' \
+               f'Clicks: {self.quantity_clicks} ' \
+               f'Uploaded by user {self.user} '
 
     class Meta:
         verbose_name = _('Activity')
